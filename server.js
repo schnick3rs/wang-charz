fcda4a6be29705094cad324501dddce69d2fd132
Moritz Orth <moritz.orth@bold-ventures.de>
2019-06-04 07:16:26 +0200

    fix dirname

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const serveStatic = require('serve-static');
 const path = require('path');
 
 const app = express();
-app.use(serveStatic(path.join(_dirname, "/dist")));
+app.use(serveStatic(path.join(__dirname, "/dist")));
 
 
 const port = process.env.PORT || 80;
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import VueRouter from "vue-router";
-import Home from "./components/Home";
 import SpeciesSelection from "./components/SpeciesSelection";
 import ArchetypeSelection from "./components/ArchetypeSelection";
 import AttributeSelection from "./components/AttributeSelection";
