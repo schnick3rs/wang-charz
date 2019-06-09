f58351edbd0fe37b2da3f7b746558c02af6ce054
Moritz Orth <moritz.orth@bold-ventures.de>
2019-06-09 02:03:27 +0200

    extended species, chapter stuff

---
+++ src/components/page/About.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+
+  <section class="about">
+    <h1>about Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+  export default  {
+    name: 'about',
+    props: [],
+    mounted() {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="css">
+  .about {
+
+  }
+</style>
