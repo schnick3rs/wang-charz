c0d94244f3c14d6a569bc7439d56e6d46a14b697
Moritz Orth <moritz.orth@bold-ventures.de>
2019-06-23 17:34:57 +0200

    hide private policy for now

--- layouts/default.vue
+++ layouts/default.vue
@@ -28,8 +28,9 @@
 
     <v-footer :app="true" class="pa-2" dark>
       <v-spacer></v-spacer>
+      <v-btn v-if="false" nuxt to="/private-policy">Private Policy</v-btn>
       <v-btn nuxt to="/about">About</v-btn>
-      <div>&copy; {{ new Date().getFullYear() }}</div>
+      <div>&copy; {{ new Date().getFullYear() }} Doctors of Doom</div>
     </v-footer>
 
   </v-app>
