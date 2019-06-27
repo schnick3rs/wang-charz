dead82f8bf87f383bbcb1af5eea06b536141f255
Moritz Orth <moritz.orth@bold-ventures.de>
2019-06-27 21:26:02 +0200

    add additional favicon for google

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ module.exports = {
     ],
     link: [
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'shortcut icon', type: 'image/x-icon', sizes: '192x192', href: '/android-chrome-192x192.png' },
       { rel: 'icon', type: 'image/x-icon', sizes: '32x32', href: '/favicon-32x32.png' },
       { rel: 'icon', type: 'image/x-icon', sizes: '16x16', href: '/favicon-16x16.png' },
       { rel: 'manifest', href: '/site.webmanifest' },
