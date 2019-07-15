b00ecf1ce56adf2b4e60650d83c1c4ec1944ffe3
Moritz Orth <moritz.orth@bold-ventures.de>
2019-07-15 21:43:09 +0200

    adjust some .env

--- api/express.js
+++ api/express.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const { Client } = require('pg');
 
-const DATABASE_URL = process.env.databaseUrl;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 const app = express();
 app.use(express.json());
