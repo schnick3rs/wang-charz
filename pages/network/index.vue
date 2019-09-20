e9f22865fba9462cbfe26d15a576b56c46e840b8
Moritz Orth <moritz.orth@bold-ventures.de>
2019-09-20 21:19:57 +0200

    vuetify-2: updated network section

--- pages/network/index.vue
+++ pages/network/index.vue
@@ -1,65 +1,94 @@
 <template>
 
-  <v-layout justify-center row wrap>
+  <div>
 
-    <v-flex
-      v-for="item in assets"
-      v-bind:key="item.name"
-      xs12
-      sm10
-      md6
-      lg6
-    >
+    <v-row justify="center">
+      <v-col :cols="11" class="elevation-4 mb-2 pa-0 ma-0">
 
-      <v-card
+      <v-breadcrumbs light
+        v-bind:items="breadcrumbItems"
       >
-        <v-card-title>
-          <h3 class="headline">{{ item.name }}</h3>
-        </v-card-title>
-
-        <v-card-text>
-          <p>{{item.text}}</p>
-        </v-card-text>
-
-        <v-list>
-
-          <v-divider></v-divider>
-
-          <v-list-tile
-            v-for="subitem in item.parts"
-            v-bind:key="subitem.title"
-            @click="trackEvent(subitem.url)"
+        <template v-slot:item="props">
+          <v-breadcrumbs-item
+            :nuxt="true"
+            :to="props.item.to"
+            :disabled="props.item.disabled"
+            :exact="props.item.exact"
           >
-            <v-list-tile-content>
-              <v-list-tile-title>{{ subitem.title }}</v-list-tile-title>
-            </v-list-tile-content>
+            <img v-if="props.item.to == '/'" src="/favicon-16x16.png" />
+            {{ props.item.text }}
+          </v-breadcrumbs-item>
+        </template>
 
-            <v-list-tile-action>
-              <v-icon>launch</v-icon>
-            </v-list-tile-action>
+        <template v-slot:divider>
+          <v-icon>mdi-chevron-right</v-icon>
+        </template>
 
-          </v-list-tile>
+      </v-breadcrumbs>
 
-        </v-list>
+      </v-col>
+    </v-row>
 
-        <v-card-actions>
-        </v-card-actions>
+    <v-row justify="center" >
 
-      </v-card>
+      <v-col
+        v-for="item in assets"
+        v-bind:key="item.name"
+        v-bind:cols="5"
+      >
 
-    </v-flex>
+        <v-card>
+          <v-card-title>
+            <h2 class="headline">{{ item.name }}</h2>
+          </v-card-title>
 
-  </v-layout>
+          <v-card-text>
+            <p>{{item.text}}</p>
+          </v-card-text>
+
+          <v-list>
+
+            <v-divider></v-divider>
+
+            <v-list-item-group>
+
+              <v-list-item
+                v-for="subitem in item.parts"
+                v-bind:key="subitem.title"
+                @click="trackEvent(subitem.url)"
+              >
+                <v-list-item-content>
+                  <v-list-item-title>{{ subitem.title }}</v-list-item-title>
+                </v-list-item-content>
+
+                <v-list-item-action>
+                  <v-icon>launch</v-icon>
+                </v-list-item-action>
+
+              </v-list-item>
+
+            </v-list-item-group>
+
+          </v-list>
+
+          <v-card-actions>
+          </v-card-actions>
+
+        </v-card>
+
+      </v-col>
+
+    </v-row>
+
+  </div>
 
 </template>
 
 <script>
 export default {
-
-  layout: 'network',
   head() {
     return {
-      title: 'W&G Assets for Players and Game-Masters | Network',
+      title: 'Wrath & Glory Assets | Network',
       meta: [
         {
           hid: 'description',
@@ -72,17 +101,25 @@ export default {
   },
   data() {
     return {
+      breadcrumbItems: [
+        {
+          text: '', nuxt: true, exact: true, to: '/',
+        },
+        {
+          text: 'Network', nuxt: true, exact: true, to: '/network',
+        },
+      ],
       assets: [
         {
           name: 'Form-Fillable Character Sheets',
           type: 'pdf',
           author: 'Deathbird',
-          text: 'Deathbird has put on some quality fillable PDF character sheets for Wrath & Glory ',
+          text: 'Deathbird has put on some quality fillable PDF character sheets for Wrath & Glory.',
           parts: [
-            { title: 'Default', url: 'https://drive.google.com/open?id=1_dcMhtBBCek_1MHEzYtJBGYtpTZoksGR' },
-            { title: 'One-Page, Psyker', url: 'https://drive.google.com/open?id=1mDtQyRW_bT4kgno5Bz7WX1HlLYXikBqh' },
-            { title: 'Two-Page, Psyker', url: 'https://drive.google.com/open?id=1rs3aZH0M_SBZzGBv5tDeWUF1m5wvGtTS' },
-            { title: 'Psychic Powers Only', url: 'https://drive.google.com/open?id=1LlHLeGdb2h3DRd_qWAaroD07pEZzPDmB' },
+            { title: 'Basis (non-psyker) sheet', url: 'https://drive.google.com/open?id=1_dcMhtBBCek_1MHEzYtJBGYtpTZoksGR' },
+            { title: 'Psyker (One additional page) sheet', url: 'https://drive.google.com/open?id=1mDtQyRW_bT4kgno5Bz7WX1HlLYXikBqh' },
+            { title: 'Psyker (Two additional pages) sheet', url: 'https://drive.google.com/open?id=1rs3aZH0M_SBZzGBv5tDeWUF1m5wvGtTS' },
+            { title: 'Psychic Powers Only sheet', url: 'https://drive.google.com/open?id=1LlHLeGdb2h3DRd_qWAaroD07pEZzPDmB' },
           ],
         }
       ],
