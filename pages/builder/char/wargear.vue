b49307469a6b0ac2859dafad8243d46c0cb98be6
Moritz Orth <moritz.orth@bold-ventures.de>
2019-09-26 08:56:12 +0200

    Rework wargear selection and allow to add custom wargear

--- pages/builder/char/wargear.vue
+++ pages/builder/char/wargear.vue
@@ -1,72 +1,31 @@
 <template lang="html" xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
 
-  <v-row justify-center row wrap>
+  <v-row justify="center" >
 
-    <v-col v-bind:cols="12" v-if="startingWargear && characterWargear.filter(g => g.source.startsWith('archetype')).length <= 0">
-
-      <h1 class="headline">Select starting Wargear</h1>
+    <v-col :cols="12">
 
       <v-card
-        v-for="gear in startingWargear.options"
-        :key="gear.key"
-        class="mb-2"
+        @click="manageWargear = !manageWargear"
+        class="mb-4"
+        dark dense outlined
+        :color=" manageWargear ? 'info' : '' "
       >
-
-        <v-card-title>
-          <span class="subtitle-1 mb-0">{{ gear.name }}</span>
-        </v-card-title>
-
-        <v-card-text v-if="gear.options && gear.options.length == 1 && gear.options[0].query">
-          <v-select
-            :items="wargearRepository.filter(gear.options[0].query)"
-            v-model="gear.selected"
-            item-value="name"
-            item-text="name"
-          ></v-select>
-
+        <v-card-text>
+          <v-icon>{{ manageWargear ? 'expand_less' : 'expand_more' }}</v-icon>
+          Manage Wargear
         </v-card-text>
-
-        <v-card-text v-else-if="gear.options">
-
-          <v-radio-group
-            v-model="gear.selected"
-            class="mt-0"
-          >
-            <v-radio
-              v-for="option in gear.options"
-              :key="option.key"
-              :label="option.name"
-              :value="option.name"
-            ></v-radio>
-          </v-radio-group>
-
-        </v-card-text>
-
       </v-card>
-
-      <v-btn
-        block
-        dense
-        color="green"
-        @click="addWargearToCharacter(startingWargear.options)"
-      >Add starting wargear</v-btn>
-
-    </v-col>
-
-    <v-col v-bind:cols="12">
-
-      <h1 class="headline">Manage Wargear</h1>
 
       <v-list
         two-line
         avatar
         dense
-        v-if="characterWargear"
+        v-if="manageWargear && characterWargear"
       >
 
         <v-list-item
           v-for="gear in characterWargear"
-          :key="gear.name"
+          :key="gear.id"
         >
 
           <v-list-item-avatar tile>
@@ -86,6 +45,109 @@
 
       </v-list>
 
+
+    </v-col>
+
+    <v-col :cols="12" >
+
+      <v-card
+        @click="startingWargearExpand = !startingWargearExpand"
+        class="mb-4"
+        dark dense outlined
+        :color=" startingWargearExpand ? 'info' : '' "
+      >
+        <v-card-text>
+          <v-icon>{{ startingWargearExpand ? 'expand_less' : 'expand_more' }}</v-icon>
+          Add Starting Wargear
+        </v-card-text>
+      </v-card>
+
+      <div v-if="startingWargearExpand">
+
+        <div v-if="startingWargear && characterWargear.filter(g => g.source.startsWith('archetype')).length <= 0">
+
+          <v-card
+            v-for="gear in startingWargear.options"
+            :key="gear.key"
+            outlined dense
+            class="mb-2"
+          >
+
+            <v-card-title>
+              <span class="subtitle-1 mb-0">{{ gear.name }}</span>
+            </v-card-title>
+
+            <v-card-text v-if="gear.options && gear.options.length == 1 && gear.options[0].query">
+              <v-select
+                :items="wargearRepository.filter(gear.options[0].query)"
+                v-model="gear.selected"
+                item-value="name"
+                item-text="name"
+              ></v-select>
+
+            </v-card-text>
+
+            <v-card-text v-else-if="gear.options">
+
+              <v-radio-group
+                v-model="gear.selected"
+                class="mt-0"
+              >
+                <v-radio
+                  v-for="option in gear.options"
+                  :key="option.key"
+                  :label="option.name"
+                  :value="option.name"
+                ></v-radio>
+              </v-radio-group>
+
+            </v-card-text>
+
+          </v-card>
+
+          <v-btn
+            v-if="startingWargearExpand"
+            block
+            dense
+            color="green"
+            @click="addWargearToCharacter(startingWargear.options)"
+          >Add starting wargear</v-btn>
+
+        </div>
+
+        <div v-else>
+          <v-card>
+            <v-card-text>
+              <v-icon>help</v-icon>
+              Starting Wargear has been added, remove all starting wargear from your inventory to reselct the starting wargear
+            </v-card-text>
+          </v-card>
+        </div>
+
+      </div>
+
+    </v-col>
+
+    <v-col :cols="12">
+
+      <v-card
+        @click="wargearSearchActive = !wargearSearchActive"
+        class="mb-4"
+        dark dense outlined
+        :color=" wargearSearchActive ? 'info' : '' "
+      >
+        <v-card-text>
+          <v-icon>{{ wargearSearchActive ? 'expand_less' : 'expand_more' }}</v-icon>
+          Add additional Wargear
+        </v-card-text>
+      </v-card>
+
+      <wargear-search
+        v-if="wargearSearchActive"
+        :repository="wargearRepository"
+        @select="add"
+      ></wargear-search>
+
     </v-col>
 
   </v-row>
@@ -93,12 +155,14 @@
 </template>
 
 <script lang="js">
-  import WargearRepositoryMixin from '~/mixins/WargearRepositoryMixin.js';
+import WargearRepositoryMixin from '~/mixins/WargearRepositoryMixin.js';
+import WargearSearch from '~/components/builder/WargearSearch.vue';
 
-  export default {
+export default {
   name: 'Wargear',
   layout: 'builder',
   mixins: [WargearRepositoryMixin],
+  components: { WargearSearch },
   props: [],
   head() {
     return {
@@ -113,7 +177,11 @@
     };
   },
   data() {
-    return {};
+    return {
+      manageWargear: true,
+      startingWargearExpand: true,
+      wargearSearchActive: false,
+    };
   },
   computed: {
     settingTier() { return this.$store.state.settingTier; },
@@ -124,24 +192,43 @@
     characterWargear() {
       const characterWargear = [];
       this.$store.state.wargear.forEach(chargear => {
-         let gear = this.wargearRepository.find(wargear => wargear.name === chargear.name);
+         var gear = {};
+         gear = this.wargearRepository.find(wargear => wargear.name === chargear.name);
          if ( gear ) {
            gear.id = chargear.id;
            gear.source = chargear.source;
-           characterWargear.push(gear);
+           console.info(`${chargear.id} > ${gear.id}`);
+           characterWargear.push({
+             id: chargear.id,
+             name: chargear.name,
+             type: this.wargearSubtitle(gear),
+             source: chargear.source,
+           });
+           console.info(characterWargear.map(i=>i.id))
          } else {
            characterWargear.push( {
              id: chargear.id,
              name: chargear.name,
              type: 'Misc',
              source: chargear.source,
-           });
+           } );
          }
       });
       return characterWargear;
     },
   },
   methods: {
+    wargearSubtitle(item) {
+      //const item = this.wargearRepository.find(i => i.name === gear);
+      if (item) {
+        let tags = [item.type];
+        if ( item.subtype ) {
+          tags.push(item.subtype);
+        }
+        return tags.filter(t=> t!== undefined).join(' • ');
+      }
+      return '';
+    },
     getAvatar(name) {
       const slug = name.toLowerCase().replace(/\s/gm, '-');
       return `/img/icon/wargear/wargear_${slug}_avatar.png`;
@@ -167,6 +254,9 @@
       finalWargear.forEach((w) => {
         this.$store.commit('addWargear', { name: w, source: 'archetype' });
       });
+    },
+    add(gear) {
+      this.$store.commit('addWargear', { name: gear.name, source: 'custom' });
     },
     remove(gear) {
       this.$store.commit('removeWargear', { id: gear.id });
