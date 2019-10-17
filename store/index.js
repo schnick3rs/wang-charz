ca7ec58f06dc29e3c4fabb596f50dc9f5f14f7d9
Moritz Orth <moritz.orth@bold-ventures.de>
2019-10-17 19:51:45 +0200

    DEV use relative URL for api calls

--- store/index.js
+++ store/index.js
@@ -3,50 +3,6 @@ import axios from 'axios';
 export const state = () => ({
   id: -1,
   setting: undefined,
-  settingSelected: true,
-  settingTier: 3,
-  customXp: 0,
-  settingHomebrewContent: [],
-  characterName: 'Simsel Simselman',
-  species: { value: undefined, cost: 0 },
-  speciesAstartesChapter: undefined,
-  archetype: { value: undefined, cost: 0 },
-  attributes: {
-    strength: 1,
-    agility: 1,
-    toughness: 1,
-    intellect: 1,
-    willpower: 1,
-    fellowship: 1,
-    initiative: 1,
-  },
-  skills: {
-    athletics: 0,
-    awareness: 0,
-    ballisticSkill: 0,
-    cunning: 0,
-    deception: 0,
-    insight: 0,
-    intimidation: 0,
-    investigation: 0,
-    leadership: 0,
-    medicae: 0,
-    persuasion: 0,
-    pilot: 0,
-    psychicMastery: 0,
-    scholar: 0,
-    stealth: 0,
-    survival: 0,
-    tech: 0,
-    weaponSkill: 0,
-  },
-  keywords: [],
-  talents: [],
-  psychicPowers: [],
-  ascensionPackages: [],
-  wargear: [],
-  background: undefined,
-  enhancements: [],
 });
 
 export const getters = {
@@ -102,19 +58,6 @@ export const getters = {
     });
     return spending;
   },
-  spendBuildingPoints(state, getters) {
-    let spend = 0;
-
-    spend += state.species ? state.species.cost : 0;
-    spend += state.archetype ? state.archetype.cost : 0;
-    spend += getters.attributeCosts;
-    spend += getters.skillCosts;
-    spend += getters.talentCosts;
-    spend += getters.ascensionCosts;
-    spend += getters.psychicPowerCosts;
-
-    return spend;
-  },
 };
 
 export const mutations = {
@@ -129,30 +72,6 @@ export const mutations = {
   setSetting(state, payload) {
     state.setting = payload.setting;
     state.settingSelected = true;
-  },
-  /**
-   * @param payload { name:String, source:String, type:String, replacement:undefined/String }
-   */
-  addKeyword(state, payload) {
-    console.log(`Adding keyword ${payload.name} of type ${payload.type}.`);
-    state.keywords.push(payload);
-  },
-  /**
-   * keyword { name:String, source:String, type:String, replacement:undefined/String }
-   * @param payload { placeholder:String, replacement:String, source:String}
-   */
-  replaceKeyword(state, payload) {
-    if ( state.keywords.length > 0) {
-      let placeholderKeyword = state.keywords.find(k => {
-        return (k.source === payload.source && k.name === payload.placeholder);
-      });
-      if ( placeholderKeyword ) {
-        placeholderKeyword.replacement = payload.replacement;
-        state.keywords = state.keywords
-          .filter( k => !(k.source === payload.source && k.name === payload.placeholder) )
-        state.keywords.push(placeholderKeyword);
-      };
-    }
   },
   addPower(state, payload) {
     const hasPower = state.psychicPowers.find(t => t.name === payload.name) !== undefined;
@@ -169,67 +88,6 @@ export const mutations = {
       state.psychicPowers = state.psychicPowers.filter( k => k.source.indexOf(payload.source) < 0 );
       console.log(`${state.psychicPowers.length} psychic powers remaining`);
     }
-  },
-  addAscension(state, payload) {
-    state.ascensionPackages.push({
-      key: payload.key,
-      value: payload.value,
-      cost: payload.cost,
-      storyElementChoice: undefined,
-      sourceTier: payload.sourceTier,
-      targetTier: payload.targetTier,
-    });
-  },
-  removeAscension(state, payload) {
-    // remove the package from the ascension stacks
-    state.ascensionPackages = state.ascensionPackages.filter(a => (a.value !== payload.value));
-
-    // remove all enhancements that are related to the package
-    state.enhancements = state.enhancements.filter(e => !e.source.startsWith(`ascension.${payload.key}`));
-
-    state.keywords = state.keywords.filter( k => k.source !== `ascension.${payload.key}`);
-
-    // ToDo: remove all wargear that is related to the package
-  },
-  setAscensionPackageStoryElement(state, payload){
-    console.info(payload);
-    // find package by payload.ascensionPackageKey and payload.ascensionPackage
-    const index = state.ascensionPackages.findIndex(a => (
-      a.key === payload.ascensionPackageKey &&
-      a.targetTier === payload.ascensionPackageTargetTier
-    ));
-    if (index >= 0) {
-      state.ascensionPackages[index].storyElementChoice = payload.ascensionPackageStoryElementKey;
-    }
-  },
-  setAscensionPackageWargearOption(state, payload){
-    console.info(`Set Ascension WargearOption to ${payload.ascensionPackageWargearOptionKey}`);
-    // find package by payload.ascensionPackageKey and payload.ascensionPackage
-    const index = state.ascensionPackages.findIndex(a => (
-      a.key === payload.ascensionPackageKey &&
-      a.targetTier === payload.ascensionPackageTargetTier
-    ));
-    if (index >= 0) {
-      state.ascensionPackages[index].wargearChoice = payload.ascensionPackageWargearOptionKey;
-    }
-  },
-  setAscensionModifications(state, payload) {
-    console.info(payload);
-    // remove previous choice
-    state.enhancements = state.enhancements.filter(e => e.source !== payload.source);
-    state.enhancements.push(payload);
-  },
-  addWargear(state, payload) {
-
-    const length = 8;
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    for (let i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    console.info(`Adding '${payload.name}' by '${payload.source}' [${result}]`)
-    state.wargear.push({ id: result, name: payload.name, source: payload.source });
   },
   setBackground(state, payload) {
     state.background = payload.name;
