0a675df5a29aff5b0e122e48718c16f8e9cd10c9
Moritz Orth <moritz.orth@bold-ventures.de>
2019-10-17 19:52:39 +0200

    DEV use relative URL for api calls

--- nuxt.config.js
+++ nuxt.config.js
@@ -93,7 +93,7 @@ module.exports = {
       '/forge/**',
     ],
     routes() {
-      const base = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000';
+      const base = process.env.NODE_ENV === 'production' ? 'https://doctors-of-doom-producti-pr-14.herokuapp.com' : 'http://localhost:3000';
       return axios.get(`${base}/api/homebrews/`)
         .then((response) => response.data.map((vaultItem) => `/vault/${vaultItem.slug}`));
     },
