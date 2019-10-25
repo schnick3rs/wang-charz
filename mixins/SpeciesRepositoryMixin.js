ac485b98717093ab26382d89fff15c958dbb1a5c
Moritz Orth <moritz.orth@bold-ventures.de>
2019-10-26 00:28:47 +0200

    replacing " -> '

--- mixins/SpeciesRepositoryMixin.js
+++ mixins/SpeciesRepositoryMixin.js
@@ -11,133 +11,133 @@ export default {
     return {
       speciesRepository: [
         {
-          key: "human",
-          name: "Human",
+          key: 'human',
+          name: 'Human',
           cost: 0,
           baseTier: 1,
           speed: 6,
-          attributes: "None",
+          attributes: 'None',
           modifications: [],
           abilities: null,
-          hint: "The humble human",
-          description: "Each human is just one of untold billions spread across the galaxy over millions of planets. Those in the Imperium live under the authority of the Emperor, but He has not stirred from His Golden Throne on Holy Terra for over ten thousand years. The remorseless government agencies of the Imperium use His authority to mercilessly rule Humanity according to their interpretations of the Emperor’s will. Every planet has its own culture and unique interpretations of the Imperium’s laws and sacred beliefs, but a few things are especially common.",
+          hint: 'The humble human',
+          description: 'Each human is just one of untold billions spread across the galaxy over millions of planets. Those in the Imperium live under the authority of the Emperor, but He has not stirred from His Golden Throne on Holy Terra for over ten thousand years. The remorseless government agencies of the Imperium use His authority to mercilessly rule Humanity according to their interpretations of the Emperor’s will. Every planet has its own culture and unique interpretations of the Imperium’s laws and sacred beliefs, but a few things are especially common.',
           avatar: null,
           theme: null
         },
         {
-          name: "Ratling",
-          key: "ratling",
+          name: 'Ratling',
+          key: 'ratling',
           cost: 15,
           baseTier: 1,
           speed: 5,
-          size: "Small",
-          attributes: "Agility +1, Strength -1",
+          size: 'Small',
+          attributes: 'Agility +1, Strength -1',
           modifications: [
-            { targetGroup: "attributes", targetValue: "strength", modifier: -1 },
-            { targetGroup: "attributes", targetValue: "agility", modifier: 1 },
+            { targetGroup: 'attributes', targetValue: 'strength', modifier: -1 },
+            { targetGroup: 'attributes', targetValue: 'agility', modifier: 1 },
           ],
-          abilities: "Abhuman,Born Sharpshooter,Conniving",
-          hint: "Half-man by name, but not by nature",
-          description: "",
+          abilities: 'Abhuman,Born Sharpshooter,Conniving',
+          hint: 'Half-man by name, but not by nature',
+          description: '',
           avatar: null,
           theme: null
         },
         {
-          key: "ogryn",
-          name: "Ogryn",
+          key: 'ogryn',
+          name: 'Ogryn',
           cost: 15,
           baseTier: 2,
           speed: 4,
-          size: "Large",
-          attributes: "Strength +2, Toughness +2, Fellowship -1, Intellect -1",
+          size: 'Large',
+          attributes: 'Strength +2, Toughness +2, Fellowship -1, Intellect -1',
           modifications: [
-            { targetGroup: "attributes", targetValue: "strength", modifier: 2 },
-            { targetGroup: "attributes", targetValue: "toughness", modifier: 2 },
-            { targetGroup: "attributes", targetValue: "fellowship", modifier: -1 },
-            { targetGroup: "attributes", targetValue: "intellect", modifier: -1 },
+            { targetGroup: 'attributes', targetValue: 'strength', modifier: 2 },
+            { targetGroup: 'attributes', targetValue: 'toughness', modifier: 2 },
+            { targetGroup: 'attributes', targetValue: 'fellowship', modifier: -1 },
+            { targetGroup: 'attributes', targetValue: 'intellect', modifier: -1 },
           ],
-          abilities: "Abhuman,Burly",
-          hint: "The brutal, eeh... human?",
-          description: "",
+          abilities: 'Abhuman,Burly',
+          hint: 'The brutal, eeh... human?',
+          description: '',
           avatar: null,
           theme: null
         },
         {
-          name: "Eldar",
-          key: "eldar",
+          name: 'Eldar',
+          key: 'eldar',
           cost: 10,
           baseTier: 1,
           speed: 8,
-          attributes: "Agility +1",
+          attributes: 'Agility +1',
           modifications: [
             {
-              targetGroup: "attributes",
-              targetValue: "agility",
+              targetGroup: 'attributes',
+              targetValue: 'agility',
               modifier: 1
             }
           ],
-          abilities: "Outsider,Intense Emotion,Psychosensitive",
-          hint: "The filthy xenos",
-          description: "The Aeldari ruled the galaxy for millions of years. During their rise to power, this species uncovered secrets of the galaxy’s very essence, learning to create and shatter entire worlds. They discovered and developed the webway, enabling them to quickly travel across the galaxy without the risk of warp travel. Their collective knowledge eventually reached the point that any drive to learn dampened, as they could accomplish virtually any task they could imagine. The longlived Aeldari existed in luxury, pursuing whatever interests drew their attention. Eventually, such leisure led to increasingly hedonistic pursuits that spread in cults across their entire population. Those moral failings, over time, coalesced within the warp, giving birth to Slaanesh.",
+          abilities: 'Outsider,Intense Emotion,Psychosensitive',
+          hint: 'The filthy xenos',
+          description: 'The Aeldari ruled the galaxy for millions of years. During their rise to power, this species uncovered secrets of the galaxy’s very essence, learning to create and shatter entire worlds. They discovered and developed the webway, enabling them to quickly travel across the galaxy without the risk of warp travel. Their collective knowledge eventually reached the point that any drive to learn dampened, as they could accomplish virtually any task they could imagine. The longlived Aeldari existed in luxury, pursuing whatever interests drew their attention. Eventually, such leisure led to increasingly hedonistic pursuits that spread in cults across their entire population. Those moral failings, over time, coalesced within the warp, giving birth to Slaanesh.',
           avatar: null,
           theme: null
         },
         {
-          name: "Ork",
+          name: 'Ork',
           cost: 10,
           baseTier: 1,
           speed: 6,
-          attributes: "Toughness +1",
+          attributes: 'Toughness +1',
           modifications: [
             {
-              targetGroup: "attributes",
-              targetValue: "toughness",
+              targetGroup: 'attributes',
+              targetValue: 'toughness',
               modifier: 1
             }
           ],
-          abilities: "Outsider,Orky,Bigger is Better",
-          key: "ork",
-          hint: "The savage brute",
-          description: "Orks are ubiquitous throughout the galaxy. Their incomprehensible physiology and ecosystem is capable of actively proliferating in even the most hostile of environments—from ash wastes, to overgrown Death Worlds, to barren asteroids. Even a small tribe of a few dozen Orks can inexplicably grow into a force of tens of thousands in relatively short order. The biological mechanisms behind this growth and adaptability confound even the most accomplished scholars of the Adeptus Mechanicus. There are no consistent requirements for Orks to thrive within any climate. However, their functionality is clearly evident on the countless worlds they have subsumed.",
+          abilities: 'Outsider,Orky,Bigger is Better',
+          key: 'ork',
+          hint: 'The savage brute',
+          description: 'Orks are ubiquitous throughout the galaxy. Their incomprehensible physiology and ecosystem is capable of actively proliferating in even the most hostile of environments—from ash wastes, to overgrown Death Worlds, to barren asteroids. Even a small tribe of a few dozen Orks can inexplicably grow into a force of tens of thousands in relatively short order. The biological mechanisms behind this growth and adaptability confound even the most accomplished scholars of the Adeptus Mechanicus. There are no consistent requirements for Orks to thrive within any climate. However, their functionality is clearly evident on the countless worlds they have subsumed.',
           avatar: null,
           theme: null
         },
         {
-          name: "Adeptus Astartes",
-          key: "adeptusAstartes",
+          name: 'Adeptus Astartes',
+          key: 'adeptusAstartes',
           cost: 50,
           baseTier: 2,
           speed: 7,
-          attributes: "Strength +1, Agility +1, Toughness +1, Resolve +1",
+          attributes: 'Strength +1, Agility +1, Toughness +1, Resolve +1',
           modifications: [
-            { targetGroup: "attributes", targetValue: "strength", modifier: 1 },
-            { targetGroup: "attributes", targetValue: "agility", modifier: 1 },
-            { targetGroup: "attributes", targetValue: "toughness", modifier: 1 },
-            { targetGroup: "traits", targetValue: "resolve", modifier: 1 }
+            { targetGroup: 'attributes', targetValue: 'strength', modifier: 1 },
+            { targetGroup: 'attributes', targetValue: 'agility', modifier: 1 },
+            { targetGroup: 'attributes', targetValue: 'toughness', modifier: 1 },
+            { targetGroup: 'traits', targetValue: 'resolve', modifier: 1 }
           ],
-          abilities: "Angel of Death,Honour the Chapter,Space Marine Implants",
-          hint: "the sword of mankind",
-          description: "Prior to launching the Great Crusade, the Emperor of Man created the Adeptus Astartes and assembled them into his legions. Each began as a mortal man, but a combination of genetic manipulations and physical implantations transformed each into a superhuman warrior—an Imperial Space Marine. The Emperor initially created twenty legions of Space Marines, each one containing vast numbers. All members of each of the twenty legions used a gene-seed developed from one of the twenty godlike Primarchs, whom the emperor also created. During the Great Crusade, Primarchs served as the generals of the Space Marine Legions, as the Emperor strove to reunify all lost human worlds, bringing the galaxy into Imperial Compliance.",
+          abilities: 'Angel of Death,Honour the Chapter,Space Marine Implants',
+          hint: 'the sword of mankind',
+          description: 'Prior to launching the Great Crusade, the Emperor of Man created the Adeptus Astartes and assembled them into his legions. Each began as a mortal man, but a combination of genetic manipulations and physical implantations transformed each into a superhuman warrior—an Imperial Space Marine. The Emperor initially created twenty legions of Space Marines, each one containing vast numbers. All members of each of the twenty legions used a gene-seed developed from one of the twenty godlike Primarchs, whom the emperor also created. During the Great Crusade, Primarchs served as the generals of the Space Marine Legions, as the Emperor strove to reunify all lost human worlds, bringing the galaxy into Imperial Compliance.',
           avatar: null,
           theme: null
         },
         {
-          name: "Primaris Astartes",
-          key: "primarisAstartes",
+          name: 'Primaris Astartes',
+          key: 'primarisAstartes',
           cost: 100,
           baseTier: 4,
           speed: 7,
-          attributes: "Strength +2, Agility +1,Toughness: +1, Resolve +1, Wounds +4",
+          attributes: 'Strength +2, Agility +1,Toughness: +1, Resolve +1, Wounds +4',
           modifications: [
-            { targetGroup: "attributes", targetValue: "strength", modifier: 2 },
-            { targetGroup: "attributes", targetValue: "agility", modifier: 1 },
-            { targetGroup: "attributes", targetValue: "toughness", modifier: 1 },
-            { targetGroup: "traits", targetValue: "resolve", modifier: 1 },
-            { targetGroup: "traits", targetValue: "wound", modifier: 4 }
+            { targetGroup: 'attributes', targetValue: 'strength', modifier: 2 },
+            { targetGroup: 'attributes', targetValue: 'agility', modifier: 1 },
+            { targetGroup: 'attributes', targetValue: 'toughness', modifier: 1 },
+            { targetGroup: 'traits', targetValue: 'resolve', modifier: 1 },
+            { targetGroup: 'traits', targetValue: 'wound', modifier: 4 }
           ],
-          abilities: "Angel of Death,Honour the Chapter (Primaris),Space Marine Implants",
-          hint: "the new breed",
-          description: "In the wake of the Horus Heresy, after completing the Codex Astartes, Primarch Roboute Guilliman decided that the Adeptus Astartes needed further enhancements to better defend the Imperium from outside threats—including those Space Marines who had turned traitor. He assigned the job of improving them to Archmagos Dominus Belisarius Cawl of the Adeptus Mechanicus. To aid the Tech-Priest, Guilliman provided him with an archive of genetic material taken from his fellow Primarchs, called the Sangprimus Portum.",
+          abilities: 'Angel of Death,Honour the Chapter (Primaris),Space Marine Implants',
+          hint: 'the new breed',
+          description: 'In the wake of the Horus Heresy, after completing the Codex Astartes, Primarch Roboute Guilliman decided that the Adeptus Astartes needed further enhancements to better defend the Imperium from outside threats—including those Space Marines who had turned traitor. He assigned the job of improving them to Archmagos Dominus Belisarius Cawl of the Adeptus Mechanicus. To aid the Tech-Priest, Guilliman provided him with an archive of genetic material taken from his fellow Primarchs, called the Sangprimus Portum.',
           avatar: null,
           theme: null
         }
@@ -145,80 +145,80 @@ export default {
       speciesAbilitiesRepository: [
         {
           key: null,
-          name: "Angel of Death",
-          effect: "Space Marines add +½ Rank icons to any successful attack against a Mob.",
+          name: 'Angel of Death',
+          effect: 'Space Marines add +½ Rank icons to any successful attack against a Mob.',
           description: null
         },
         {
           key: null,
-          name: "Honour the Chapter",
-          effect: "You are subject to the orders of your chapter master, and must honour both the beliefs and traditions of your chapter.",
+          name: 'Honour the Chapter',
+          effect: 'You are subject to the orders of your chapter master, and must honour both the beliefs and traditions of your chapter.',
           description: null
         },
         {
           key: null,
-          name: "Space Marine Implants",
-          effect: "Space Marines do not bleed. Space Marines gain +1 bonus dice as a situational modifi er to any test if the Game Master deems it appropriate for one of the 19 implants.",
+          name: 'Space Marine Implants',
+          effect: 'Space Marines do not bleed. Space Marines gain +1 bonus dice as a situational modifi er to any test if the Game Master deems it appropriate for one of the 19 implants.',
           description: null
         },
         {
           key: null,
-          name: "Honour the Chapter (Primaris)",
-          effect: "You are subject to the orders of your chapter master and the beliefs and traditions of your chapter. Primaris Space Marines gain the fi rst special rule from their Chapter and ignore any penalties or drawbacks from the second if it is marked as being related to the Chapter’s gene-seed.",
+          name: 'Honour the Chapter (Primaris)',
+          effect: 'You are subject to the orders of your chapter master and the beliefs and traditions of your chapter. Primaris Space Marines gain the fi rst special rule from their Chapter and ignore any penalties or drawbacks from the second if it is marked as being related to the Chapter’s gene-seed.',
           description: null
         },
         {
           key: null,
-          name: "Outsider",
-          effect: "+2DN to all Interaction tests with those with the Keyword <Imperium>.",
+          name: 'Outsider',
+          effect: '+2DN to all Interaction tests with those with the Keyword <Imperium>.',
           description: null
         },
         {
           key: null,
-          name: "Abhuman",
-          effect: "+1DN to all interaction tests with characters possessing the Imperium keyword.",
+          name: 'Abhuman',
+          effect: '+1DN to all interaction tests with characters possessing the Imperium keyword.',
           description: null
         },
         {
           key: null,
-          name: "Burly",
-          effect: " +2d to all Intimidation tests.",
+          name: 'Burly',
+          effect: ' +2d to all Intimidation tests.',
           description: null
         },
         {
           key: null,
-          name: "Born Sharpshooter",
-          effect: " +2d to all Ballistic Skill tests.",
+          name: 'Born Sharpshooter',
+          effect: ' +2d to all Ballistic Skill tests.',
           description: null
         },
         {
           key: null,
-          name: "Conniving",
-          effect: "Ratlings begin play with the Scum keyword.",
+          name: 'Conniving',
+          effect: 'Ratlings begin play with the Scum keyword.',
           description: null
         },
         {
           key: null,
-          name: "Orky",
-          effect: "+1 to all Intimidation tests.",
+          name: 'Orky',
+          effect: '+1 to all Intimidation tests.',
           description: null
         },
         {
           key: null,
-          name: "Bigger is Better",
-          effect: "Orks calculate Infl uence using their Strength in place of Fellowship.",
+          name: 'Bigger is Better',
+          effect: 'Orks calculate Infl uence using their Strength in place of Fellowship.',
           description: null
         },
         {
           key: null,
-          name: "Intense Emotion",
-          effect: "+1DN to all Resolve tests. Failing a Willpower-based test in a scene involving intense emotion grants the GM +1 Ruin.",
+          name: 'Intense Emotion',
+          effect: '+1DN to all Resolve tests. Failing a Willpower-based test in a scene involving intense emotion grants the GM +1 Ruin.',
           description: null
         },
         {
           key: null,
-          name: "Psychosensitive",
-          effect: "All Eldar may purchase 1 Minor Psychic Power if they also purchase the Psychic Mastery skill. This purchase also gives them the Psyker keyword. In addition, the Tier Restriction for Maximum Psychic Powers for Eldar Characters is increased by 1 to accommodate this purchase.",
+          name: 'Psychosensitive',
+          effect: 'All Eldar may purchase 1 Minor Psychic Power if they also purchase the Psychic Mastery skill. This purchase also gives them the Psyker keyword. In addition, the Tier Restriction for Maximum Psychic Powers for Eldar Characters is increased by 1 to accommodate this purchase.',
           description: null
         }
       ],
