cb22c41a1ab0f15a5c4e1031df1712a69159a906
brother-lucius <moritz.orth@bold-ventures.de>
2019-12-18 21:39:12 +0100

    feature(posts): remove and change access key

--- plugins/contentful-inject.js
+++ plugins/contentful-inject.js
@@ -1,13 +1,12 @@
 // ~/plugins/contentful-inject.js
-require('dotenv').config();
 import { createClient } from 'contentful';
 
-export default ({ app }, inject) => {
+export default ({ app, env }, inject) => {
   // use default environment config for convenience
   // these will be set via `env` property in nuxt.config.js
   const config = {
-    space: process.env.CTF_SPACE_ID,
-    accessToken: process.env.CTF_CDA_ACCESS_TOKEN,
+    space: env.CTF_SPACE_ID,
+    accessToken: env.CTF_CD_ACCESS_TOKEN,
   };
   app.myContentful = createClient(config);
 }
