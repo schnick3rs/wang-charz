394728fb26fd2c76f326323d6ba142711dd06b81
brother-lucius <moritz.orth@bold-ventures.de>
2020-04-25 09:52:13 +0200

    feature(forge): more powers

--- api/db/static/psychicPowersRepository.js
+++ api/db/static/psychicPowersRepository.js
@@ -21,6 +21,21 @@ const stringToKebab = function (text) {
   return text.toLowerCase().replace(/\W/gm, '-');
 };
 
+const powerz = function(sourceKey, sourcePage, name, discipline, cost, effect, stub = false) {
+  return {
+    source: {
+      ...source[sourceKey],
+      page: sourcePage,
+    },
+    key: `${stringToKebab(`${sourceKey} ${name}`)}`,
+    name,
+    cost,
+    discipline,
+    effect,
+    stub,
+  };
+};
+
 const simpleStub = function (id, sourceKey, sourcePage, cost, name, discipline, effect) {
   return {
     source: {
@@ -48,6 +63,195 @@ const simpleCrunch = function (dn, activation, duration, range, multi, effect, p
     crunch_potency: potency.split(';'),
   };
 };
+
+const corePowers = [
+  {
+    ...powerz('core',268,'Chameleon','Minor',8),
+    ...simpleCrunch(5,'Action','Sustained','Self',false),
+    keywords: ['Psychic'],
+    effect: '+3 to Stealth and +1 Defence vs Ranged Attacks.',
+    description: '<p>You bend reality just enough to cause your image to blend with your surroundings. While this power remains in effect, you gain +3 bonus dice to Stealth (A) Tests and + 1 to your Defence against ranged attacks.</p>',
+  },
+  {
+    ...powerz('core',268,'Compel','Minor',10),
+    ...simpleCrunch(5,'Simple Action','1 Round','5 m',false),
+    keywords: ['Psychic'],
+    effect: 'Target must pass DN 4 Wil test or must follow a single command.',
+  },
+  {
+    ...powerz('core',268,'Conceal Phenomena','Minor',5),
+    ...simpleCrunch(3,'Full-Round Action','Sustained','Self',false),
+    keywords: ['Psychic'],
+    effect: 'Psychic atempts to detect you are at +2 DN.',
+  },
+  {
+    ...powerz('core',269,'Conjure Flame','Minor',10),
+    ...simpleCrunch(4,'Movement','Sustained','Self',false),
+    keywords: ['Fire','Psychic'],
+    effect: 'Flaming Melee Attacks that deal 8+1ED and may (GM) inflict On Fire',
+  },
+  {
+    ...powerz('core',269,'Dull Pain','Minor',8),
+    ...simpleCrunch(4,'Simple Action','1 Round','5 m',true),
+    keywords: ['Psychic'],
+    effect: 'Affected targets reduce all shock they suffer by 1.',
+  },
+  {
+    ...powerz('core',269,'Flash Bang','Minor',8),
+    ...simpleCrunch(4,'Action','Instant','5 m',true),
+    keywords: ['Auditory','Light','Psychic'],
+    effect: 'Targets in range must pass DN 3 Tou of suffer 1 shock and be blinded.',
+  },
+  {
+    ...powerz('core',269,'Invoke Luck','Minor',10),
+    ...simpleCrunch(4,'Simple Action','1 Round','Self',false),
+    keywords: ['Psychic'],
+    effect: 'You may gain +1 die to one Test you make.',
+  },
+  {
+    ...powerz('core',269,'Inflict Pain','Minor',8),
+    ...simpleCrunch(4,'Action','Sustained','5 m',true),
+    keywords: ['Psychic'],
+    effect: 'Deal 1d3 shock, target must pass DN3 Wil or be staggered.',
+  },
+  {
+    ...powerz('core',270,'Subvert Machine','Minor',10),
+    ...simpleCrunch(4,'Action','Instant','25 m',true),
+    keywords: ['Kinetic','Psychic'],
+    effect: 'Jam machine for 1 minute. Might be fixed with DN 3 Tech (Int) Test.',
+  },
+  {
+    ...powerz('core',270,'Hover','Minor',7),
+    ...simpleCrunch(4,'Simple Action','Sustained','Self',false),
+    keywords: ['Psychic'],
+    effect: 'Float freely at half speed.',
+  },
+  {
+    ...powerz('core',270,'Psychic Torch','Minor',5),
+    ...simpleCrunch(4,'Simple Action','Sustained','Self',false),
+    keywords: ['Kinetic','Psychic'],
+    effect: 'Create an illuminating, flying orb.',
+  },
+  {
+    ...powerz('core',270,'Phantom Grip','Minor',8),
+    ...simpleCrunch(4,'Full Action','Sustained','10 m',false),
+    keywords: ['Kinetic','Psychic'],
+    effect: 'Manifest phantom hands under your control.',
+  },
+  {
+    ...powerz('core',270,'Mental Force','Minor',8),
+    ...simpleCrunch(3,'Action','Instant','15 m',false),
+    keywords: ['Kinetic','Psychic'],
+    effect: 'Do a mental flex to push the target prone.',
+  },
+  {
+    ...powerz('core',271,'Otherworlldy Voices','Minor',5),
+    ...simpleCrunch(3,'Action','Sustained','25 m',false),
+    keywords: ['Auditory','Psychic'],
+    effect: 'Disturb others with you AWESOME WARP VOICE.',
+  },
+  // Universal
+  {
+    ...powerz('core',272,'Smite','Universal',10),
+    ...simpleCrunch('Defence','Action','Instant','35 m',true),
+    keywords: ['Psychic'],
+    effect: 'Roll Psychic Mastery VS Defence to deal 1d3 Mortal Wounds.',
+  },
+  // Biomancy
+  {
+    ...powerz('core',272,'Enfeeble','Biomancy',15),
+    ...simpleCrunch('Defence','Action','Sustained','10 m',true),
+    keywords: ['Psychic'],
+    effect: 'The target’s Strength is reduced by 1 and they suffer 1 Shock at the beginning of each of their turns while the power is sustained.',
+  },
+  {
+    ...powerz('core',272,'Life Leech','Biomancy',15),
+    ...simpleCrunch('Defence','Action','Instant','5 m',false),
+    keywords: ['Psychic'],
+    effect: 'Deal 1d6 shock and 1d3 Wounds, heal half of the inflicted amount.',
+  },
+  {
+    ...powerz('core',272,'Warp Speed','Biomancy',15),
+    ...simpleCrunch(7,'Action','Sustained','Self',false),
+    keywords: ['Psychic'],
+    effect: 'Double your speed, make an additional Action per turn, +1 Defence, always act first in combat round. Suffer 1d3+1 Shock each round.',
+  },
+  {
+    ...powerz('core',272,'Phantom Form','Biomancy',15),
+    ...simpleCrunch(7,'Action','Sustained','Self',false),
+    keywords: ['Psychic'],
+    effect: 'Move through solid matter at half speed. Suffer 1 Shock each round.',
+  },
+  {
+    ...powerz('core',273,'Regeneration','Biomancy',15),
+    ...simpleCrunch(8,'Action','Sustained','Self',false),
+    keywords: ['Psychic'],
+    effect: 'Heal one Wound per round or - if completely healed - regain 1 Shock per round.',
+  },
+  {
+    ...powerz('core',273,'Sharp Flesh','Biomancy',20),
+    ...simpleCrunch(8,'Action','Sustained','Self',false),
+    keywords: ['Psychic'],
+    prerequisite: ['At least one other Biomancy Power.'],
+    effect: 'Change your form, you filthy shapechanger.',
+  },
+  // Divination
+  {
+    ...powerz('core',274,'Forewarding','Divination',15),
+    ...simpleCrunch(6,'Full Action','1 Combat','Self',false),
+    keywords: ['Psychic'],
+    effect: 'Seize initiative for free and gain +1 Defence.',
+  },
+  //
+  {
+    ...powerz('core',283,'Conceal / Reveal','Runes of Battle',20),
+    ...simpleCrunch(5,'Action','Sustained','25 m',true),
+    keywords: ['Aeldari','Psychic'],
+    prerequisite: ['Psyker must have the AELDARI Keyword.'],
+    hint: 'You reach out with your mind and take command of the shadows, bending and shaping them to your will.',
+    effect: 'Concealed allies within 5m gain +1 to Defence and Stealth. Revealed targets can not benefit from Cover.',
+  },
+  {
+    ...powerz('core',283,'Embolden / Horrofy','Runes of Battle',20),
+    ...simpleCrunch(5,'Action','Sustained','25 m',false),
+    keywords: ['Aeldari','Psychic'],
+    prerequisite: ['Psyker must have the AELDARI Keyword.'],
+    hint: 'You reach into the minds of those around you, either fortifying or eroding their resolve.',
+    effect: 'Gain +1 to resolve or cause +1 DN to resolve tests.',
+  },
+  {
+    ...powerz('core',283,'Empower / Enervate','Runes of Battle',30),
+    ...simpleCrunch(7,'Action','Sustained','25 m',false),
+    keywords: ['Aeldari','Psychic'],
+    prerequisite: ['Psyker must have the AELDARI Keyword.'],
+    hint: 'You commune with the spirits of those around you, expanding their potential or draining their will to fight.',
+    effect: 'Gain +1 ED in melee or cause -1 ED in melee.',
+  },
+  {
+    ...powerz('core',284,'Enhance / Drain','Runes of Battle',30),
+    ...simpleCrunch(7,'Action','Sustained','25 m',false),
+    keywords: ['Aeldari','Psychic'],
+    prerequisite: ['Psyker must have the AELDARI Keyword.'],
+    hint: 'Drawing upon the favour of Khaine, you enhance the combat prowess of your allies or diminish that of your enemies.',
+    effect: 'Gain +1 to Weapon Skill or suffer +1 DN to Weapon Skill.',
+  },
+  {
+    ...powerz('core',284,'Protect / Jinx','Runes of Battle',20),
+    ...simpleCrunch(5,'Action','Sustained','25 m',false),
+    keywords: ['Aeldari','Psychic'],
+    prerequisite: ['Psyker must have the AELDARI Keyword.'],
+    hint: 'Tugging on the threads of fate that surround any field of battle, you isolate and bind those of a specific individual, reshaping their destiny.',
+    effect: 'Gain +1 to Resilience or suffer -1 to Resilience',
+  },
+  {
+    ...powerz('core',284,'Quicken / Restrain','Runes of Battle',20),
+    ...simpleCrunch(7,'Action','Sustained','25 m',false),
+    keywords: ['Aeldari','Psychic'],
+    prerequisite: ['Psyker must have the AELDARI Keyword.'],
+    hint: 'You twist time itself, briefly altering its flow for a select few on the battlefield.',
+    effect: 'Gain additional movement or apply difficult terrain.',
+  },
+];
 
 const paxNavigatorPowers = [
   {
@@ -394,6 +598,7 @@ const teaLibrariusPowers = [
 ];
 
 const psychicPowersRepository = [
+  ...corePowers,
   ...paxNavigatorPowers,
   ...aaoaWaaaghPowers,
   ...aaoaSancticPowers,
--- api/routes/psychicPowers.js
+++ api/routes/psychicPowers.js
@@ -3,7 +3,7 @@ const Router = require('express-promise-router');
 const db = require('../db');
 const { sourceSql } = require('./_sqlSnippets');
 
-const psychicPowersRepository = require('../db/static/psychicPowersRepository');
+const repository = require('../db/static/psychicPowersRepository');
 
 const router = new Router();
 
@@ -12,7 +12,7 @@ module.exports = router;
 const TABLE_FIELDS = ['id', 'name', 'cost', 'keywords', 'effect', 'discipline'];
 
 router.get('/', async (request, response) => {
-  let select = '*';
+
   const fieldsString = request.query.fields;
   if (fieldsString) {
     const selectFields = [];
@@ -24,7 +24,7 @@ router.get('/', async (request, response) => {
     select = selectFields.join(',');
   }
 
-  let homebrewPowers = psychicPowersRepository;
+  let homebrewPowers = repository;
 
   let where = ' WHERE 1=1';
   const filter = {};
@@ -33,7 +33,6 @@ router.get('/', async (request, response) => {
   if (filterDisciplineString) {
     filter.discipline = filterDisciplineString.split(',');
     if (filter.discipline) {
-      where += ` AND discipline in ( '${filter.discipline.join("','")}' )`;
       homebrewPowers = homebrewPowers.filter( (p) => filter.discipline.includes(p.discipline));
     }
   }
@@ -42,17 +41,13 @@ router.get('/', async (request, response) => {
   if (filterNameString) {
     filter.name = filterNameString.split(',');
     if (filter.name) {
-      where += ` AND name in ( '${filter.name.join("','")}' )`;
       homebrewPowers = homebrewPowers.filter( (p) => filter.name.includes(p.name));
     }
   }
 
   // for now, all powers are from the CORE rules, replace with 'p.source_id' once added
-  const query = `select p.*, ${sourceSql('1')} as source from wrath_glory.psychic_powers p ${where}`;
-  const { rows } = await db.queryAsyncAwait(query, []);
 
   let merged = [
-    ...rows,
     ...homebrewPowers,
   ];
 
@@ -68,14 +63,10 @@ router.get('/', async (request, response) => {
   response.status(200).json(merged);
 });
 
-router.get('/:id', async (request, response) => {
-  const { id } = request.params;
+router.get('/:slug', async (request, response) => {
+  const { slug } = request.params;
 
-  // for now, all powers are from the CORE rules, replace with 'p.source_id' once added
-  const query = `select p.*, ${sourceSql('1')} as source from wrath_glory.psychic_powers p WHERE p.id = $1 LIMIT 1`;
-  const { rows } = await db.queryAsyncAwait(query, [id]);
-
-  const item = rows[0];
+  const item = repository.find(power => power.key === slug);
 
   response.set('Cache-Control', 'public, max-age=3600'); // one hour
   response.status(200).json(item);
