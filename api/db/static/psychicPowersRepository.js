a11eba6697e448936c97bb3c71f12d046fe86470
brother-lucius <moritz.orth@bold-ventures.de>
2020-06-16 19:36:25 +0200

    feature(forge): add AAOA ork psychic powers

--- api/db/static/psychicPowersRepository.js
+++ api/db/static/psychicPowersRepository.js
@@ -2,7 +2,7 @@ const source = {
   core: { book: 'Core Rules (v1.5)', key: 'core', version: 'v1.5' },
   core10: { book: 'Core Rules (v1.0)', key: 'core10', version: 'v1' },
   coreab: { book: 'Abhumans (Beta)', key: 'coreab', version: 'v0.5' },
-  aaoa: { book: 'An Abundance of Apocrypha (v3)', sumplements: 'core', key: 'aaoa', version: 'v3', path: '/vault/an-abundance-of-apocrypha' },
+  aaoa: { book: 'An Abundance of Apocrypha (v3.1)', sumplements: 'core', key: 'aaoa', version: 'v3.1', path: '/vault/an-abundance-of-apocrypha' },
   aaoa2: { book: 'An Abundance of Apocrypha (v2)', sumplements: 'core10', key: 'aaoa2', version: 'v2', path: '/vault/an-abundance-of-apocrypha' },
   lotn: { book: 'Legacy of the Necrontyr', key: 'lotn', version: '', path: '/vault/legacy-of-the-necrontyr' },
   thaot: { book: 'The High Altar of Technology', key: 'thaot', version: '', path: '/vault/the-high-altar-of-technology' },
@@ -531,63 +531,46 @@ const paxNavigatorPowers = [
 
 const aaoaWaaaghPowers = [
   {
-    ...simpleStub(200, 'aaoa2', 181, 20, '‘Eadbanger', 'WAAAGH!', 'Bring the closest enemy to zero wounds.'),
-    ...simpleCrunch('8', 'Action', 'Instant', '35m', false, ''),
+    ...simpleStub(200, 'aaoa', 233, 20, '‘Eadbanger', 'WAAAGH!', 'Bring the closest enemy to zero wounds.'),
+    ...simpleCrunch('7', 'Action', 'Instant', '18m', false, ''),
     keywords: ['Ork','Psychic'],
     description:
-      '<p>A bolt of raw power erupts from the Weirdboy’s forehead and rockets across the battlefield, ' +
-      'causing the head of the first unfortunate victim caught in its path to explode in a shower of brains and gore.</p>' +
-      '<p>This power targets the closest enemy, who must pass a Toughness test (DN 3) or reduced to 0 Wounds immediately.</p>',
+      '<p>A bolt of raw power erupts from your head and rockets across the battlefield, causing the head of the first unfortunate victim caught in its path to explode in a shower of brains and gore. This power targets the closest enemy, who must pass a Toughness test (DN 3) immediately suffer enough wounds to exceed their Max Wounds by 1.</p>',
   },
   {
-    ...simpleStub(201, 'aaoa2', 181, 15, 'Da Jump', 'WAAAGH!', 'Short Teloport yourself and allied orks.'),
-    ...simpleCrunch('7', 'Full Action', 'Instant', '25m', false, ''),
+    ...simpleStub(201, 'aaoa', 233, 15, 'Da Jump', 'WAAAGH!', 'Short Teloport yourself and allied orks.'),
+    ...simpleCrunch('6', 'Full Action', 'Instant', '12m', false, ''),
     keywords: ['Ork','Psychic'],
     description:
-      '<p>The Weirdboy closes his eyes tight and, in a storm of flashing green light, teleports a mass of confused greenskins to another part of the battlefield. ' +
-      'Select a number of allies with the Ork keyword equal to the psyker’s Willpower within range. ' +
-      'Those allies vanish, and then reappear moments later anywhere within 500m. ' +
-      'They must reappear more than 20m from an enemy, and they forfeit their move action in their following turns.</p>',
+      '<p>You close your eyes tight and, in a storm of flashing green light, you teleport a mass of confused greenskins to another part of the battlefield. Select a number of allies with the ORK keyword equal to your Willpower within range. Those allies vanish, and then reappear moments later anywhere within 100m. They must reappear more than 9m from an enemy, and they forfeit their move in their following turn.</p>',
   },
   {
-    ...simpleStub(202, 'aaoa2', 181, 15, 'Da Krunch', 'WAAAGH!', 'Stomp enemies with mortal force and knock them prone.'),
-    ...simpleCrunch('8', 'Action', 'Instant', '35m', false, ''),
+    ...simpleStub(202, 'aaoa', 233, 15, 'Da Krunch', 'WAAAGH!', 'Stomp enemies with mortal force and knock them prone.'),
+    ...simpleCrunch('7', 'Action', 'Instant', '18m', false, ''),
     keywords: ['Ork','Psychic'],
     description:
-      '<p>Green energies erupt form the Weirdboy’s eyes, ears, nose, and mouth, and coalesce into a roiling cloud above the enemy. ' +
-      'That cloud then solidifies into the vast green foot of Gork (or Mork) himself, which commences to stamp down upon the foe. ' +
-      'This power affects all enemies in a Medium blast within range. ' +
-      'Affected enemies take 1d3 Mortal Wounds and are knocked prone.' +
-      ' Foes who were already prone become staggered as well. ' +
-      'Then, roll a d6: on a roll of a 6, repeat the power’s effects as the foot stamps down again.</p>',
+      '<p>Green energies erupt from your eyes, ears, nose, and mouth, and coalesce into a roiling cloud above the enemy. That cloud then solidifies into the vast green foot of Gork (or Mork) himself, which commences to stamp down upon the foe. This power affects all enemies in a Medium blast within range. Affected enemies take 1d3 Mortal Wounds and are knocked prone. Foes who were already prone become staggered as well. Then, roll a d6: on an Exalted Icon, repeat the power’s effects (repositioning the blast as desired) as the foot stamps down again.</p>',
   },
   {
-    ...simpleStub(203, 'aaoa2', 181, 8, 'Fists of Gork', 'WAAAGH!', 'Boost yourself or an allie with great Strength.'),
-    ...simpleCrunch('6', 'Action', '1 Round', '25m', false, ''),
+    ...simpleStub(203, 'aaoa', 233, 8, 'Fists of Gork', 'WAAAGH!', 'Boost yourself or an allie with great Strength.'),
+    ...simpleCrunch('5', 'Action', '1 Round', '12m', false, ''),
     keywords: ['Ork','Psychic'],
     description:
-      '<p>The Weirdboy channels Waaagh energy into his own fists or those of another Ork, providing strength enough to punch through the armour of tanks. ' +
-      'This power affects the psyker or one ally with the Ork keyword within range. ' +
-      'Until the end of the psyker’s next turn, the affected character’s Strength is increased by +4 and they receive +2d on all melee attacks.</p>',
+      '<p>You channel Waaagh energy into your own fists or those of another Ork, providing strength enough to punch through the armour of tanks. This power affects you or one ally with the ORK keyword within range. Until the end of your next turn, the affected character’s Strength is increased by +4 and they receive +2 bonus dice on all melee attacks.</p>',
   },
   {
-    ...simpleStub(204, 'aaoa2', 182, 10, 'Roar of Mork', 'WAAAGH!', 'Enemies within range reduce their resolve by 2.'),
-    ...simpleCrunch('8', 'Full Action', 'Sustained', '35m', false, ''),
+    ...simpleStub(204, 'aaoa', 234, 10, 'Roar of Mork', 'WAAAGH!', 'Enemies within range reduce their resolve by 2.'),
+    ...simpleCrunch('7', 'Full Action', 'Sustained', '18m', false, ''),
     keywords: ['Ork','Telepathy','Psychic'],
     description:
-      '<p>The Weirdboy opens his gob impossibly wide and gives vent to a bellowing roar that reverberates through his enemies’ minds. ' +
-      'Coherent thought becomes nigh-impossible, and as the roar thunders on, panic begins to spread. ' +
-      'While this power remains in effect, all enemies within range reduce their Resolve by 2.</p>',
+      '<p>You open your gob impossibly wide and give vent to a bellowing roar that reverberates through your enemies’ minds. Coherent thought becomes nigh-impossible, and as the roar thunders on, panic begins to spread. While this power remains in effect, all enemies within range add +2 DN to all Resolve tests.</p>',
   },
   {
-    ...simpleStub(205, 'aaoa2', 182, 10, 'Warpath', 'WAAAGH!', 'Allies within range increase strength and make more multi-actions or attacks.'),
-    ...simpleCrunch('7', 'Action', 'Instant', '35m', false, ''),
+    ...simpleStub(205, 'aaoa', 234, 15, 'Warpath', 'WAAAGH!', 'Allies within range increase strength and make more multi-actions or attacks.'),
+    ...simpleCrunch('6', 'Action', 'Instant', '18m', false, ''),
     keywords: ['Ork','Psychic'],
     description:
-      '<p>The Weirdboy disperses the Waaagh energy coursing through his frame into the Ork warriors around him, ' +
-      'stoking their already bellicose nature into a roaring fever pitch. Select a number of allies with the Ork keyword within range. ' +
-      'The affected allies add +2d to all melee attacks they attempt until the start of the psyker’s next turn, ' +
-      'and they may ignore up to 2 points of DN increase for taking the Multi-Attack or Multi-Action options.</p>',
+      '<p>You disperse the Waaagh energy coursing through your frame into the Ork warriors around you, stoking their already bellicose nature into a roaring fever pitch. Select a number of allies with the ORK keyword equal to your Willpower within range. The affected allies add +2 bonus dice to all melee attacks they attempt until the start of your next turn, and they may ignore up to 2 points of DN increase for taking the Multi-Attack or Multi-Action options.</p>',
   },
 ];
 
