aa70e6c1e3b8d0446c990d6520adb3bb6bdb254c
brother-lucius <moritz.orth@bold-ventures.de>
2020-12-06 17:56:39 +0100

    feature(forge): add vault twitter card

--- pages/vault/_slug.vue
+++ pages/vault/_slug.vue
@@ -173,6 +173,11 @@ export default {
           name: 'keywords',
           content: keywords.join(','),
         },
+        // Twitter Card
+        { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+        { hid: 'twitter:title', name: 'twitter:title', content: title },
+        { hid: 'twitter:description', name: 'twitter:description', content: description },
+        { hid: 'twitter:image', name: 'twitter:image', content: image },
       ],
       __dangerouslyDisableSanitizers: ['script'],
       script: [
