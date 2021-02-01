fc2cea15a7ea3dcaeda923d56be5b1198b4155b1
brother-lucius <moritz.orth@bold-ventures.de>
2021-02-01 23:58:34 +0100

    feature(forge): CHERRY PICK mechdendrites are very Rare

--- api/db/static/wargearRepository.js
+++ api/db/static/wargearRepository.js
@@ -1155,27 +1155,27 @@ const core = [
     modifications: [ { targetGroup: 'attributes', targetValue: 'intellect', modifier: 1 } ],
   },
   {
-    ...gear(source.core.key,244,'Mechadendrites (Ballistic)','5R','Adeptus Mechanicus'),
+    ...gear(source.core.key,244,'Mechadendrites (Ballistic)','5V','Adeptus Mechanicus'),
     type: 'Augmetics', subtype: 'Augmetic Implants', triptype: 'Mechadendrites',
     snippet: 'Functions as a Laspistol (p.217). Your biology powers the weapon, so it does not use Ammo or need to be Reloaded.',
   },
   {
-    ...gear(source.core.key,245,'Mechadendrites (Exploration)','5R','Adeptus Mechanicus'),
+    ...gear(source.core.key,245,'Mechadendrites (Exploration)','5V','Adeptus Mechanicus'),
     type: 'Augmetics', subtype: 'Augmetic Implants', triptype: 'Mechadendrites',
     snippet: 'Functions as an Auspex (p.236). +1 bonus die to Survival (Wil) Tests to navigate or track.',
   },
   {
-    ...gear(source.core.key,245,'Mechadendrites (Medicae)','5R','Adeptus Mechanicus'),
+    ...gear(source.core.key,245,'Mechadendrites (Medicae)','5V','Adeptus Mechanicus'),
     type: 'Augmetics', subtype: 'Augmetic Implants', triptype: 'Mechadendrites',
     snippet: 'Functions as a Medikit and a Diagnostor (p.238 & p.237). +1 bonus die to Medicae (Int) Tests. Can be used to inject toxins, sedatives, and stimulants in combat as a Simple Injecting an unwilling target requires an Opposed Initiative Test.',
   },
   {
-    ...gear(source.core.key,245,'Mechadendrites (Optical)','5R','Adeptus Mechanicus'),
+    ...gear(source.core.key,245,'Mechadendrites (Optical)','5V','Adeptus Mechanicus'),
     type: 'Augmetics', subtype: 'Augmetic Implants', triptype: 'Mechadendrites',
     snippet: 'Functions as a Pict Recorder and Magnoculars (p.237). +1 bonus die to Awareness (Int) Tests; allows you to make microscopic examinations.',
   },
   {
-    ...gear(source.core.key,245,'Mechadendrites (Servo-Arm)','5R','Adeptus Mechanicus'),
+    ...gear(source.core.key,245,'Mechadendrites (Servo-Arm)','5V','Adeptus Mechanicus'),
     type: 'Augmetics', subtype: 'Augmetic Implants', triptype: 'Mechadendrites',
     snippet: 'You gain +4 Strength when using the arm. In combat, the arm allows you to Brace (p.189) as a Free Action. You can use the arm as a melee weapon with the following profile: Damage 6+2ED, AP-3, Unwieldy(2)',
     meta: [
@@ -1183,7 +1183,7 @@ const core = [
     ],
   },
   {
-    ...gear(source.core.key,245,'Mechadendrites (Utility)','5R','Adeptus Mechanicus'),
+    ...gear(source.core.key,245,'Mechadendrites (Utility)','5V','Adeptus Mechanicus'),
     type: 'Augmetics', subtype: 'Augmetic Implants', triptype: 'Mechadendrites',
     snippet: 'Functions as a Combi-Tool (p.236). +1 bonus die to Tech (Int) Tests.',
   },
