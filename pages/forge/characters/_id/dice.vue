aa7614a176674a2af99007884b315b3019c5684b
brother-lucius <moritz.orth@bold-ventures.de>
2021-03-23 23:25:43 +0100

    feature(dice-roller): basics

--- pages/forge/characters/_id/dice.vue
+++ pages/forge/characters/_id/dice.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
-    <v-btn @click="roll()">
+    <v-btn @click="animate()">
       Roll
     </v-btn>
-    <div id="ThreeJS" style="position: absolute; left:0px; top:0px"></div>
+    <div id="container" style="height: 400px; width: 600px; background-color: #3b8070;"></div>
   </div>
 </template>
 
 <script>
 //import THREE, { OrbitControls } from 'three/build/three';
-import { DiceManager, DiceD6 } from 'threejs-dice/lib/dice';
-import * as THREE from 'three';
+import * as Three from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { BoxLineGeometry } from 'three/examples/jsm/geometries/BoxLineGeometry';
 import Stats from 'three/examples/jsm/libs/stats.module';
@@ -18,58 +17,44 @@ import Stats from 'three/examples/jsm/libs/stats.module';
 
 export default {
   name: 'dice',
+  mounted() {
+    this.init();
+
+  },
+  data() {
+    return {
+      camera: undefined,
+      scene: undefined,
+      mesh: undefined,
+      renderer: undefined,
+    };
+  },
   methods: {
-    roll() {
-      let scene = new THREE.Scene();
+    init() {
+      let container = document.getElementById('container');
+      this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight);
+      this.camera.position.z = 1;
 
-      // CAMERA
-      let SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
-      let VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
-      let camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
-      scene.add(camera);
+      this.scene = new Three.Scene();
+      let geometry = new Three.BoxGeometry(0.5,0.5,1);
+      let material = new Three.MeshBasicMaterial({color: 0xff0000});
 
-      // RENDERER
-      let renderer = new THREE.WebGLRenderer( {antialias:true} );
-      renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-      let container = document.getElementById( 'ThreeJS' );
-      container.appendChild( renderer.domElement );
-      // EVENTS
-      // CONTROLS
-      let controls = new OrbitControls( camera, renderer.domElement );
-      camera.position.set(100, 500, 300);
-      camera.rotation.x = -0.95;
-      // STATS
-      let stats = new Stats();
-      stats.domElement.style.position = 'absolute';
-      stats.domElement.style.bottom = '0px';
-      stats.domElement.style.zIndex = 100;
-      container.appendChild( stats.domElement );
-      // LIGHT
-      var light = new THREE.PointLight(0xffffff, 0.8);
-      light.position.set(0,150,100);
-      scene.add(light);
-      // LIGHT
-      var light = new THREE.PointLight(0xffffff, 0.8);
-      light.position.set(-400,150,100);
-      scene.add(light);
-      // LIGHT
-      var light = new THREE.PointLight(0xffffff, 0.8);
-      light.position.set(400,150,100);
-      scene.add(light);
-      // FLOOR
-      var floorMaterial = new THREE.MeshPhongMaterial( { color: '#00aa00', side: THREE.DoubleSide } );
-      var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
-      var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-      floor.position.y = -0.5;
-      floor.rotation.x = Math.PI / 2;
-      scene.add(floor);
-      // SKYBOX/FOG
-      var skyBoxGeometry = new BoxLineGeometry( 10000, 10000, 10000 );
-      var skyBoxMaterial = new THREE.MeshPhongMaterial( { color: 0x9999ff, side: THREE.BackSide } );
-      var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
-      // scene.add(skyBox);
-      scene.fog = new THREE.FogExp2( 0x9999ff, 0.00025 );
-    }
+      this.mesh = new Three.Mesh(geometry, material);
+      this.mesh.position.z = -5;
+      this.mesh.rotation.x = 10;
+      this.mesh.rotation.y = 5;
+      this.scene.add(this.mesh);
+
+      this.renderer = new Three.WebGLRenderer({antialias: true});
+      this.renderer.setSize(container.clientWidth, container.clientHeight);
+      container.appendChild(this.renderer.domElement);
+    },
+    animate() {
+      requestAnimationFrame(this.animate);
+      this.mesh.rotation.x += 0.01;
+      this.mesh.rotation.y += 0.02;
+      this.renderer.render(this.scene, this.camera);
+    },
   },
 }
 </script>
