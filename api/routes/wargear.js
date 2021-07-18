05fa1ac4e0578ea0ccf2c2897dac996e06da9e8a
brother-lucius <moritz.orth@bold-ventures.de>
2021-07-19 01:02:25 +0200

    feature(forge): add foundry link armour

--- api/routes/wargear.js
+++ api/routes/wargear.js
@@ -187,7 +187,7 @@ function toFoundry(item) {
           "salvo": rangedWeapon.salvo || 0,
           "traits": rangedWeapon.traits.join(', '),
           "value": parseInt(item.value) || 0,
-          "rarity": rangedWeapon.rarity,
+          "rarity": item.rarity,
           "keywords": item.keywords.map(k => k.toUpperCase()).join(', '),
           "upgrades": ""
         },
@@ -287,7 +287,7 @@ function toFoundry(item) {
           "salvo": 0,
           "traits": meleeWeapon.traits.join(', '),
           "value": parseInt(item.value) || 0,
-          "rarity": meleeWeapon.rarity,
+          "rarity": item.rarity,
           "keywords": item.keywords.map(k => k.toUpperCase()).join(', '),
           "upgrades": ""
         },
@@ -302,6 +302,75 @@ function toFoundry(item) {
           }
         }
       };
+    case 'Armour':
+      const armour = item.meta.find(i => i.type === 'armour');
+      return {
+        "name": item.name,
+        "type": "armour",
+        "img": "icons/svg/mystery-man.svg",
+        "data": {
+          "bonus": {
+            "attributes": {
+              "strength": 0,
+              "toughness": 0,
+              "agility": 0,
+              "initiative": 0,
+              "willpower": 0,
+              "intellect": 0,
+              "fellowship": 0
+            },
+            "skills": {
+              "athletics": 0,
+              "awareness": 0,
+              "ballisticSkill": 0,
+              "cunning": 0,
+              "deception": 0,
+              "insight": 0,
+              "intimidation": 0,
+              "investigation": 0,
+              "leadership": 0,
+              "medicae": 0,
+              "persusasion": 0,
+              "pilot": 0,
+              "psychicMastery": 0,
+              "scholar": 0,
+              "stealth": 0,
+              "survival": 0,
+              "tech": 0,
+              "weaponSkill": 0
+            },
+            "combat": {
+              "defense": 0,
+              "resilence": 0,
+              "wounds": 0,
+              "determination": 0,
+              "shock": 0,
+              "resolve": 0,
+              "passiveAwareness": 0
+            }
+          },
+          "description": item.hint,
+          "category": "armour",
+          "rating": armour.armourRating,
+          "traits": armour.traits.join(', '),
+          "value": parseInt(item.value) || 0,
+          "rarity": item.rarity,
+          "keywords": item.keywords.map(k => k.toUpperCase()).join(', '),
+          "upgrades": ""
+        },
+        "effects": [],
+        "sort": 0,
+        "flags": {
+          "exportSource": {
+            "world": "wrath-and-glory",
+            "system": "wrath-and-glory",
+            "coreVersion": "0.8.6",
+            "systemVersion": "2.1.0"
+          }
+        }
+      };
+    default:
+      return [];
   }
 }
 
