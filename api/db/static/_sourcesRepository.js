651a3d139cdc02539facb6c11106f14378460518
brother-lucius <moritz.orth@bold-ventures.de>
2022-01-02 00:16:57 +0100

    feature(forge): adding AAOA Arbites Enforcer

--- api/db/static/_sourcesRepository.js
+++ api/db/static/_sourcesRepository.js
@@ -10,7 +10,7 @@ const source = {
   gohe: { book: 'Godless Heathens', key: 'gohe', version: 'v0.1', path: '' },
   soti: { book: 'Shadow of the Imperium Sandbox', key: 'soti', version: 'v0.1' },
   // Homebrews (Up to date)
-  aaoa: { book: 'An Abundance of Apocrypha', key: 'aaoa', version: 'v6.0', path: '/vault/an-abundance-of-apocrypha' },
+  aaoa: { book: 'An Abundance of Apocrypha', key: 'aaoa', version: 'v7.0', path: '/vault/an-abundance-of-apocrypha' },
   aaoa2: { book: 'An Abundance of Apocrypha', key: 'aaoa2', version: 'v2.0', path: '/vault/an-abundance-of-apocrypha' },
   tog: { book: 'Tome of Glory', key: 'tog', version: '', path: '/vault/tome-of-glory' },
   amb: { book: 'Astra Militarum Brew', key: 'amb', version: '', path: '/vault/astra-militarum-brew' },
