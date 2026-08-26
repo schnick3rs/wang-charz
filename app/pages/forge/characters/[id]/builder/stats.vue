445a32b132aff3e0f5b3ebd1ee9c990a8c10fc98
Moritz Halke <moritz.halke@ella-lab.io>
2026-08-26 10:32:47 +0200

    stats

--- app/pages/forge/characters/[id]/builder/stats.vue
+++ app/pages/forge/characters/[id]/builder/stats.vue
@@ -1,9 +1,105 @@
 <script setup lang="ts">
+import {useCharacterStore} from "~~/stores/characters.ts";
+import {attributeRepository, skillRepository, traitRepository} from "#shared/utils/stats.ts";
 
+definePageMeta({ layout: 'forge' })
+
+const route = useRoute()
+const id = computed(() => route.params.id as string)
+const store = useCharacterStore()
+
+const entity = computed(() => store.byId[id.value])
+
+//TODO load species and apply max values
+function effectiveTrait(trait: any) {
+
+  if (trait.key === 'speed') return 'X'
+
+  let baseValue = 0
+
+  if (trait.attribute) {
+    const associatedAttribute = attributeRepository.find((a) => a.name === trait.attribute)
+    baseValue = entity.value.data.attributes[associatedAttribute.key]
+  }
+
+  if (trait.skill) {
+    const associatedSkill = skillRepository.find((a) => a.name === trait.skill)
+    baseValue = entity.value.data.skills[associatedSkill.key]
+  }
+
+  const { static: statik, multi, tier, min } = trait.compute
+
+  const computedValue = statik + ( baseValue * multi ) + (tier * entity.value.data.settingTier )
+
+  return Math.ceil(Math.max(computedValue, min))
+}
 </script>
 
 <template>
+  <div class="mx-auto max-w-3xl">
 
+    <h1 class="font-bold text-2xl">Select Attributes & Skills</h1>
+
+    <div v-if="entity" class="grid grid-cols-1 md:grid-cols-2 gap-4">
+
+      <UCard :ui="{ body: 'flex flex-col gap-2' }">
+
+        <div v-for="attribute in attributeRepository" :key="attribute.key" class="flex items-center justify-between">
+          <span>{{ $t(`stats.${attribute.key}`, attribute.name) }}</span>
+          <UInputNumber
+              v-model="entity.data.attributes[attribute.key]"
+              :min="1"
+              :increment="{
+              color: 'primary',
+              variant: 'subtle',
+              size: 'xs'
+            }"
+              :decrement="{
+              color: 'error',
+              variant: 'subtle',
+              size: 'xs'
+            }"
+              class="w-32"
+          />
+        </div>
+
+        <USeparator></USeparator>
+
+        <div>
+          <div v-for="trait in traitRepository" :key="trait.key" class="flex items-center justify-between gap-2">
+            <span>{{ $t(`stats.${trait.key}`, trait.name) }}</span>
+            <span class="mx-15 my-1">{{ effectiveTrait(trait) }}</span>
+          </div>
+        </div>
+
+      </UCard>
+
+      <UCard :ui="{ body: 'flex flex-col gap-2' }">
+
+        <div v-for="skill in skillRepository" :key="skill.key" class="flex items-center justify-between">
+          <span>{{ $t(`stats.${skill.key}`, skill.name) }}</span>
+          <UInputNumber
+              v-model="entity.data.skills[skill.key]"
+              :min="1"
+              :max="6"
+              :increment="{
+              color: 'primary',
+              variant: 'subtle',
+              size: 'xs',
+            }"
+              :decrement="{
+              color: 'error',
+              variant: 'subtle',
+              size: 'xs',
+            }"
+              class="w-32"
+          />
+        </div>
+      </UCard>
+
+    </div>
+
+  </div>
 </template>
 
 <style scoped>
---
+++ shared/utils/stats.ts
@@ -0,0 +1,287 @@
+export type CharacterAttributes = {
+    strength: number;
+    toughness: number;
+    agility: number;
+    initiative: number;
+    willpower: number;
+    intellect: number;
+    fellowship: number;
+};
+
+export type AttributeKey = keyof CharacterAttributes;
+
+interface AttributeDefinition {
+    key: AttributeKey;
+    name: string;
+    short: string;
+    description: string;
+    order: number;
+}
+
+export const attributeRepository: AttributeDefinition[] = [
+    {
+        key: 'strength',
+        name: 'Strength',
+        short: 'Str',
+        description: 'Raw physical power.',
+        order: 1,
+    },
+    {
+        key: 'toughness',
+        name: 'Toughness',
+        short: 'Tou',
+        description: 'Endurance and ability to shrug off damage.',
+        order: 3,
+    },
+    {
+        key: 'agility',
+        name: 'Agility',
+        short: 'Agi',
+        description: 'Dexterity and coordination.',
+        order: 2,
+    },
+    {
+        key: 'initiative',
+        name: 'Initiative',
+        short: 'Ini',
+        description: 'Reflexes and reaction speed.',
+        order: 7,
+    },
+    {
+        key: 'willpower',
+        name: 'Willpower',
+        short: 'Wil',
+        description: 'Determination and strength of will.',
+        order: 5,
+    },
+    {
+        key: 'intellect',
+        name: 'Intellect',
+        short: 'Int',
+        description: 'Ability to process and interpret information.',
+        order: 4,
+    },
+    {
+        key: 'fellowship',
+        name: 'Fellowship',
+        short: 'Fel',
+        description: 'Force of personality.',
+        order: 6,
+    },
+];
+
+export const skillRepository = [
+    {
+        key: 'athletics',
+        name: 'Athletics',
+        attribute: 'Strength',
+        description: 'A character’s overall physical prowess',
+    },
+    {
+        key: 'awareness',
+        name: 'Awareness',
+        attribute: 'Intellect',
+        description: 'Notice additional details, or perceive hidden or obscured objects.',
+    },
+    {
+        key: 'ballisticSkill',
+        name: 'Ballistic Skill',
+        attribute: 'Agility',
+        description: undefined,
+    },
+    {
+        key: 'cunning',
+        name: 'Cunning',
+        attribute: 'Fellowship',
+        description: undefined,
+    },
+    {
+        key: 'deception',
+        name: 'Deception',
+        attribute: 'Fellowship',
+        description: undefined,
+    },
+    {
+        key: 'insight',
+        name: 'Insight',
+        attribute: 'Fellowship',
+        description: undefined,
+    },
+    {
+        key: 'intimidation',
+        name: 'Intimidation',
+        attribute: 'Willpower',
+        description: undefined,
+    },
+    {
+        key: 'investigation',
+        name: 'Investigation',
+        attribute: 'Intellect',
+        description: undefined,
+    },
+    {
+        key: 'leadership',
+        name: 'Leadership',
+        attribute: 'Willpower',
+        description: undefined,
+    },
+    {
+        key: 'medicae',
+        name: 'Medicae',
+        attribute: 'Intellect',
+        description: undefined,
+    },
+    {
+        key: 'persuasion',
+        name: 'Persuasion',
+        attribute: 'Fellowship',
+        description: 'convince',
+    },
+    {
+        key: 'pilot',
+        name: 'Pilot',
+        attribute: 'Agility',
+        description: undefined,
+    },
+    {
+        key: 'psychicMastery',
+        name: 'Psychic Mastery',
+        attribute: 'Willpower',
+        description: undefined,
+    },
+    {
+        key: 'scholar',
+        name: 'Scholar',
+        attribute: 'Intellect',
+        description: undefined,
+    },
+    {
+        key: 'stealth',
+        name: 'Stealth',
+        attribute: 'Agility',
+        description: undefined,
+    },
+    {
+        key: 'survival',
+        name: 'Survival',
+        attribute: 'Willpower',
+        description: undefined,
+    },
+    {
+        key: 'tech',
+        name: 'Tech',
+        attribute: 'Intellect',
+        description: undefined,
+    },
+    {
+        key: 'weaponSkill',
+        name: 'Weapon Skill',
+        attribute: 'Initiative',
+        description: undefined,
+    },
+];
+
+
+// https://api.sheety.co/2d702477-7a22-4d71-9c25-6119ee216253
+export const traitRepository = [
+    {
+        key: 'defence',
+        name: 'Defence',
+        attribute: 'Initiative',
+        description: undefined,
+        type: 'Combat',
+        compute: { static: -1, multi: 1, tier: 0, min: 1 },
+    },
+    {
+        key: 'resilience',
+        name: 'Resilience',
+        attribute: 'Toughness',
+        description: undefined,
+        type: 'Combat',
+        compute: { static: 1, multi: 1, tier: 0, min: 1 },
+    },
+    {
+        key: 'determination',
+        name: 'Determination',
+        attribute: 'Toughness',
+        description: undefined,
+        type: 'Combat',
+        compute: { static: 0, multi: 1, tier: 0, min: 1 },
+    },
+    {
+        key: 'maxWounds',
+        name: 'Max Wounds',
+        attribute: 'Toughness',
+        description: undefined,
+        type: 'Combat',
+        compute: { static: 0, multi: 1, tier: 2, min: 1 },
+    },
+    {
+        key: 'maxShock',
+        name: 'Max Shock',
+        attribute: 'Willpower',
+        description: undefined,
+        type: 'Combat',
+        compute: { static: 0, multi: 1, tier: 1, min: 1 },
+    },
+    {
+        key: 'conviction',
+        name: 'Conviction',
+        attribute: 'Willpower',
+        description: undefined,
+        type: 'Mental',
+        compute: { static: 0, multi: 1, tier: 0, min: 1 },
+    },
+    {
+        key: 'resolve',
+        name: 'Resolve',
+        attribute: 'Willpower',
+        description: undefined,
+        type: 'Mental',
+        compute: { static: -1, multi: 1, tier: 0, min: 1 },
+    },
+    {
+        key: 'influence',
+        name: 'Influence',
+        attribute: 'Fellowship',
+        description: undefined,
+        type: 'Social',
+        compute: { static: -1, multi: 1, tier: 0, min: 1 },
+        calculate: (base: number) => {
+            return base-1;
+        },
+    },
+    {
+        key: 'wealth',
+        name: 'Wealth',
+        attribute: undefined,
+        description: undefined,
+        type: 'Social',
+        compute: { static: 0, multi: 1, tier: 1, min: 0 },
+    },
+    {
+        key: 'speed',
+        name: 'Speed',
+        attribute: undefined,
+        description: undefined,
+        type: 'Combat',
+        compute: { static: 0, multi: 1, tier: 0, min: 0 },
+    },
+    {
+        key: 'corruption',
+        name: 'Corruption',
+        attribute: undefined,
+        description: undefined,
+        type: 'Mental',
+        compute: { static: 0, multi: 1, tier: 0, min: 0 },
+    },
+    {
+        key: 'passiveAwareness',
+        name: 'Passive Awareness',
+        attribute: undefined,
+        skill: 'Awareness',
+        description: undefined,
+        type: 'Mental',
+        compute: { static: 0, multi: 0.5, tier: 0, min: 1 },
+    },
+];
